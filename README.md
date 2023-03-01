9a0a606b5dd7fa4ab09b2acb7692534646de7de5
Gabe Busto <gbusto.gator@gmail.com>
2023-03-01 12:42:57 -0500

    Updated README and script to allow for switching between IBFT and Clique
    IBFT isn't fully working yet though. There is an issue where miners are
    not "authorized" so no blocks get mined. I have some ideas I'm still
    working on to figure out how to fix this. But I wanted to keep the
    script the same as much as possible. It should be possible to pretty
    much do exactly the same things for the majority of the setup process
    and just change the underlying consensus being used.

--- README.md
+++ README.md
@@ -9,9 +9,87 @@ source myenv/bin/activate
 pip3 install -r requirements.txt
 ```
 
+Run the following commands to setup quorum:
+```
+brew install go
+git clone https://github.com/Consensys/quorum.git
+cd quorum
+make all
+```
+
+Ensure that `$PATH` contained `geth` and `bootnode` from the previous step installing Quorum (you'll need to use a modified version of geth).
+
 Next, install geth. If you're on a Mac like me and have Homebrew installed, you can simply run `brew install geth`
 
-After that, all that's left is to run `python private-eth.py`
+After that, all that's left is to run `python private-eth-network.py`
+
+## Q&A
+### What is the tmp/ directory?
+This is the output from the `quorum-genesis-tool` command when following instructions on [this](https://docs.goquorum.consensys.net/tutorials/private-network/create-ibft-network) page. I have it in here for reference for the time being while I try to massage Quorum's IBFT consensus into my current script, and it might be useful for others as well. It will be removed in the future.
+
+### What is the scripts/ directory?
+These are some short scripts that I've used to test various things and automate some simple processes. I would ideally like to turn this whole script into a shell/terminal of some kind to be used as a local tool for quickly spinning up and managing nodes for testing things. The functionality provided by the scripts would then be available through the shell as a command to automate something within the test network.
+
+### What consensus protocols are supported?
+Quorum's IBFT and Clique PoA.
+
+### How do I change the consensus protocol being used in this script?
+For now, edit `private-eth-network.py` manually at the top of the file. Change the `CONSENSUS` variable.
+
+### How can I connect to a node locally?
+From inside of this directory, you can connect to a node by using a command like this: `geth attach nodes/miners/mnode1/geth.ipc`
+
+### How can I tell if a node is running or see its command output?
+You can monitor a node through a terminal by running a command like this: `tail -f nodes/miners/mnode1/proc.out`
+
+The `proc.out` file in each node directory will contain live output for each running node. If a node is not running, you'll see something like a `Fatal` error in the logs in `proc.out` indicating that the process stopped, using `ps aux | grep geth` to see if any geth processes are running, or try connecting to the local unix socket `geth attach nodes/miners/mnode1/proc.out` and see if the connection fails (if it fails, the process is likely not running).
+
+### What should the directory structure look like for the nodes/ directory if things are setup properly?
+You should see something similar to what's shown below:
+```
+nodes/miners/mnode1
+nodes/miners/mnode1/geth
+nodes/miners/mnode1/geth/nodes
+nodes/miners/mnode1/geth/nodes/000001.log
+nodes/miners/mnode1/geth/nodes/MANIFEST-000000
+nodes/miners/mnode1/geth/nodes/LOCK
+nodes/miners/mnode1/geth/nodes/CURRENT
+nodes/miners/mnode1/geth/nodes/LOG
+nodes/miners/mnode1/geth/LOCK
+nodes/miners/mnode1/geth/chaindata
+nodes/miners/mnode1/geth/chaindata/000003.log
+nodes/miners/mnode1/geth/chaindata/000002.ldb
+nodes/miners/mnode1/geth/chaindata/LOCK
+nodes/miners/mnode1/geth/chaindata/ancient
+nodes/miners/mnode1/geth/chaindata/ancient/receipts.cidx
+nodes/miners/mnode1/geth/chaindata/ancient/bodies.0000.cdat
+nodes/miners/mnode1/geth/chaindata/ancient/bodies.cidx
+nodes/miners/mnode1/geth/chaindata/ancient/diffs.0000.rdat
+nodes/miners/mnode1/geth/chaindata/ancient/receipts.0000.cdat
+nodes/miners/mnode1/geth/chaindata/ancient/hashes.ridx
+nodes/miners/mnode1/geth/chaindata/ancient/FLOCK
+nodes/miners/mnode1/geth/chaindata/ancient/headers.0000.cdat
+nodes/miners/mnode1/geth/chaindata/ancient/hashes.0000.rdat
+nodes/miners/mnode1/geth/chaindata/ancient/diffs.ridx
+nodes/miners/mnode1/geth/chaindata/ancient/headers.cidx
+nodes/miners/mnode1/geth/chaindata/CURRENT
+nodes/miners/mnode1/geth/chaindata/LOG
+nodes/miners/mnode1/geth/chaindata/MANIFEST-000004
+nodes/miners/mnode1/geth/chaindata/CURRENT.bak
+nodes/miners/mnode1/geth/nodekey
+nodes/miners/mnode1/geth/transactions.rlp
+nodes/miners/mnode1/geth/lightchaindata
+nodes/miners/mnode1/geth/lightchaindata/000001.log
+nodes/miners/mnode1/geth/lightchaindata/MANIFEST-000000
+nodes/miners/mnode1/geth/lightchaindata/LOCK
+nodes/miners/mnode1/geth/lightchaindata/CURRENT
+nodes/miners/mnode1/geth/lightchaindata/LOG
+nodes/miners/mnode1/keystore
+nodes/miners/mnode1/keystore/UTC--2023-03-01T16-59-52.671508000Z--63e6dc99e25b66727d3970ac89e6cd250a056def
+nodes/miners/mnode1/geth.ipc
+nodes/miners/mnode1/proc.out
+// Many more nodes after this one
+```
 
 ## Learnings
 * [This](https://geth.ethereum.org/docs/fundamentals/private-network) tutorial for setting up a private PoA network is great. The two things I missed while glancing through this that prevented my network from working correctly are 1) not paying attention to the fact that `extradata` in the genesis file should contain the addresses of all initial signers of the network, and 2) not specifying the `--mine` parameter in the geth command to start the signing nodes. After this, blocks were being mined and I could actually submit transactions
@@ -27,6 +105,5 @@ After that, all that's left is to run `python private-eth.py`
 * If you see the error `unauthorized signer` in the `proc.out` or command output for a node after starting the network up, it likely means that no signers were specified in the `extradata` field of the `genesis.json` file
 * You can connect to a node locally using IPC. If that connection fails, it's likely because that node has stopped for some reason and the process is no longer running
 * Calling `miner.start()` when there are no nodes in the network doesn't work as expected. I believe you need to specify signers initially and set them up to mine out of the gate. Then on any new signers you want to add to the network, you can call `miner.start()` via the geth console on them to get them mining also. If you start your network off with signers and don't give them the `--mine` parameter, then calling `miner.start()` doesn't kick things off the right way. This is because you need a certain number of specified signers in the network, and the Clique protocol requires that signers rotate. The error I was seeing in this case was something like `err="signed recently, must wait for others"`.
-* ChatGPT was actually pretty helpful in troubleshooting some of this stuff!
-
-
+* ~~ChatGPT was actually pretty helpful in troubleshooting some of this stuff!~~ ChatGPT was somewhat helpful in talking through these things. But it was wrong or unclear on some parts of the Clique PoA protocol.
+* More learnings coming soon...
--- private-eth-network.py
+++ private-eth-network.py
@@ -7,9 +7,21 @@ import json
 import os
 import shutil
 
+import rlp
+
+#CONSENSUS = "clique"
+CONSENSUS = "ibft"
+
+SYSTEM_GETH_BIN = "/usr/local/bin/geth"
+MODIFIED_GETH_BIN = "./quorum/build/bin/geth"
+
+GETH_BIN = SYSTEM_GETH_BIN
+if CONSENSUS == "ibft":
+    GETH_BIN = MODIFIED_GETH_BIN
+
 config = {
-    "period": 15,
-    "epoch": 300,
+    "period": 5,
+    "epoch": 30000,
     "chainId": 12345,
     "passwordFile": "password.txt",
     "initialBalance": "10000000000000000000",
@@ -22,7 +34,7 @@ config = {
     "memberNodesRoot": os.path.join("nodes", "members"),
 }
 
-genesisJson = {
+genesisJsonClique = {
   "config": {
     "chainId": 12345,
     "homesteadBlock": 0,
@@ -50,6 +62,53 @@ genesisJson = {
   }
 }
 
+genesisJsonIBFT = {
+  "nonce": "0x0",
+  "timestamp": "0x0",
+  # In the Clique genesis file, this is called "extradata" (all lowercase)
+  "extraData": "",
+  "gasLimit": "0xFFFFFF",
+  "gasUsed": "0x0",
+  "number": "0x0",
+  "difficulty": "0x1",
+  "coinbase": "0x0000000000000000000000000000000000000000",
+  # Fixed magic number for Istanbul block identification
+  # https://github.com/ethereum/EIPs/issues/650
+  "mixHash": "0x63746963616c2062797a616e74696e65206661756c7420746f6c6572616e6365",
+  "parentHash": "0x0000000000000000000000000000000000000000000000000000000000000000",
+  "config": {
+    "chainId": config.get("chainId"),
+    "homesteadBlock": 0,
+    "eip150Block": 0,
+    "eip150Hash": "0x0000000000000000000000000000000000000000000000000000000000000000",
+    "eip155Block": 0,
+    "eip158Block": 0,
+    "byzantiumBlock": 0,
+    "constantinopleBlock": 0,
+    "petersburgBlock": 0,
+    "istanbulBlock": 0,
+    "muirglacierblock": 0,
+    "berlinBlock": 0,
+    "londonBlock": 0,
+    "isQuorum": True,
+    "maxCodeSizeConfig": [
+      {
+        "block": 0,
+        "size": 64
+      }
+    ],
+    "txnSizeLimit": 64,
+    "ibft": {
+      "policy": 0,
+      "epoch": config.get("epoch"),
+      "ceil2Nby3Block": 0,
+      "blockperiodseconds": config.get("period")
+    }
+  },
+  "alloc": {
+  }
+}
+
 def makeDirs(directories):
     for directory in directories:
         os.mkdir(directory)
@@ -66,7 +125,7 @@ def _executeCommand(cmd):
 
 def initNodeDirs(directories, password):
     for directory in directories:
-        cmd = "geth --datadir {} account new".format(directory)
+        cmd = "{} --datadir {} account new".format(GETH_BIN, directory)
         printCommand(cmd)
         proc = pexpect.spawn(cmd)
         while True:
@@ -89,7 +148,7 @@ def initNodeDirs(directories, password):
 
 def gethInitGenesis(directories):
     for directory in directories:
-        cmd = "geth init --datadir {} genesis.json".format(directory)
+        cmd = "{} init --datadir {} genesis.json".format(GETH_BIN, directory)
         printCommand(cmd)
         proc = _executeCommand(cmd)
         proc.wait()
@@ -119,7 +178,15 @@ def parseBootnodeOutput(proc):
 
     return ""
 
-def startNodes(dirsAddrsZipped, enodeString, passwordFilePath):
+def startNodes(consensus, dirsAddrsZipped, enodeString, passwordFilePath):
+    if consensus == "ibft":
+        _func = startNodesIBFT
+    else:
+        _func = startNodesClique
+
+    return _func(dirsAddrsZipped, enodeString, passwordFilePath)
+
+def startNodesClique(dirsAddrsZipped, enodeString, passwordFilePath):
     # geth --datadir node1 --port 30306 --bootnodes enode://f7aba85ba369923bffd3438b4c8fde6b1f02b1c23ea0aac825ed7eac38e6230e5cadcf868e73b0e28710f4c9f685ca71a86a4911461637ae9ab2bd852939b77f@127.0.0.1:0?discport=30305  --networkid 123454321 --unlock 0xC1B2c0dFD381e6aC08f34816172d6343Decbb12b --password node1/password.txt --authrpc.port 8551
     basePort = 30306
     baseRpcPort = 9550
@@ -132,13 +199,44 @@ def startNodes(dirsAddrsZipped, enodeString, passwordFilePath):
         address = dirsAddrsZipped[i][1]
 
         if "miner" in directory:
-            cmd = "geth --datadir {} --port {} --bootnodes \"{}\" --networkid 12345 --unlock 0x{} --password {} --authrpc.port {} --mine".format(
-                        directory, basePort + i, enodeString, address, passwordFilePath, baseRpcPort
+            cmd = "{} --datadir {} --port {} --bootnodes \"{}\" --networkid 12345 --unlock 0x{} --password {} --authrpc.port {} --mine".format(
+                        GETH_BIN, directory, basePort + i, enodeString, address, passwordFilePath, baseRpcPort
                     )
             baseRpcPort += 1
         else:
-            cmd = "geth --datadir {} --port {} --bootnodes \"{}\" --networkid 12345 --authrpc.port {} --http --http.addr 127.0.0.1 --http.port {}".format(
-                        directory, basePort + i, enodeString, baseRpcPort + i, baseHttpPort
+            cmd = "{} --datadir {} --port {} --bootnodes \"{}\" --networkid 12345 --authrpc.port {} --http --http.addr 127.0.0.1 --http.port {}".format(
+                        GETH_BIN, directory, basePort + i, enodeString, baseRpcPort + i, baseHttpPort
+                    )
+            baseHttpPort += 1
+
+        printCommand(cmd)
+
+        filename = os.path.join(directory, "proc.out")
+        with open(filename, "w") as f:
+            proc = Popen(cmd, shell=True, stdout=f, stderr=f)
+            procs.append(proc)
+
+    return procs
+
+def startNodesIBFT(dirsAddrsZipped, enodeString, passwordFilePath):
+    basePort = 30306
+    baseRpcPort = 9550
+    baseHttpPort = 8545
+
+    procs = []
+
+    for i in range(len(dirsAddrsZipped)):
+        directory = dirsAddrsZipped[i][0]
+        address = dirsAddrsZipped[i][1]
+
+        if "miner" in directory:
+            cmd = "{} --datadir {} --syncmode full --port {} --bootnodes \"{}\" --networkid 12345 --unlock 0x{} --password {} --mine --miner.threads 1 --miner.gasprice 0 --emitcheckpoints --allow-insecure-unlock".format(
+                        GETH_BIN, directory, basePort + i, enodeString, address, passwordFilePath
+                    )
+            baseRpcPort += 1
+        else:
+            cmd = "{} --datadir {} --syncmode full --port {} --bootnodes \"{}\" --networkid 12345 --http --http.addr 127.0.0.1 --http.port {} --allow-insecure-unlock".format(
+                        GETH_BIN, directory, basePort + i, enodeString, baseHttpPort
                     )
             baseHttpPort += 1
 
@@ -173,15 +271,57 @@ def findKeystorePathForNode(directory):
 
     return ""
 
+def createExtraDataClique(addresses):
+    extraData = "0x" + "00" * 32
+
+    for address in addresses:
+        extraData += address
+    
+    extraData += "00" * 65
+
+    return extraData
+    
+def createExtraDataIBFT(addresses):
+    old_string = "f841"
+    new_string = "b841"
+
+    for i in range(0, 65):
+        old_string = old_string + "80"
+        new_string = new_string + "00"
+
+    vanity = "0x0000000000000000000000000000000000000000000000000000000000000000"
+
+    seal = []
+
+    for i in range(0, 65):
+        seal.append(0x00)
+
+    committed_seal = []
+
+    validators = []
+
+    for address in addresses:
+        validators.append(int("0x" + address, 16))
+
+    extra_data = vanity + rlp.encode([validators, seal, committed_seal]).hex()
+
+    extra_data = extra_data.replace(old_string,  new_string)
+
+    return extra_data
+
 def createGenesisFile(genesisJson, minerAddresses, memberAddresses):
     for address in minerAddresses + memberAddresses:
         genesisJson["alloc"][address] = { "balance": config.get("initialBalance") }
 
-    extradata = "0x" + "00" * 32
-    for address in minerAddresses:
-        extradata += address
-    extradata += "00" * 65
-    genesisJson["extradata"] = extradata
+    # Unclear if this is significant yet, but in the clique genesis, its "extradata" (not camelcase)
+    # In the IBFT genesis, its "extraData" (camelcase)
+
+    if genesisJson.get("config").get("ibft", None) is not None:
+        extraData = createExtraDataIBFT(minerAddresses)
+        genesisJson["extraData"] = extraData
+    else:
+        extraData = createExtraDataClique(minerAddresses)
+        genesisJson["extradata"] = extraData
 
     with open("genesis.json", "w") as f:
         json.dump(genesisJson, f)
@@ -212,6 +352,8 @@ def cleanup(directories, bootnodeProcess, nodeProcesses):
     removeFile("genesis.json")
 
 if __name__ == "__main__":
+    print("[+] Consensus is {}".format(CONSENSUS))
+
     passwordFilename = config.get("passwordFile")
     bootnodeProcess = None
     nodeProcesses = []
@@ -229,6 +371,13 @@ if __name__ == "__main__":
 
         minerAddresses, memberAddresses = getNodeAddresses(combinedDirs)
 
+        if CONSENSUS == "ibft":
+            print("[+] Using IBFT genesis JSON")
+            genesisJson = genesisJsonIBFT
+        else:
+            print("[+] Using Clique genesis JSON")
+            genesisJson = genesisJsonClique
+            
         createGenesisFile(genesisJson, minerAddresses, memberAddresses)
 
         gethInitGenesis(combinedDirs)
@@ -242,7 +391,7 @@ if __name__ == "__main__":
         addresses = minerAddresses + memberAddresses
 
         dirsAddrsZipped = list(zip(combinedDirs, addresses))
-        nodeProcesses = startNodes(dirsAddrsZipped, enode, passwordFilename)
+        nodeProcesses = startNodes(CONSENSUS, dirsAddrsZipped, enode, passwordFilename)
     except Exception as e:
         print("ERROR!")
         print(e)
